80fe4461767a4b149c4e0c1984fdea301eeb8de9
caapricorn <caapricorn@mail.ru>
2021-12-18 03:42:49 +0300

    Scripted auto-commit on change (2021-12-18 03:42:49) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -8,4 +8,11 @@ public class StorageActor extends AbstractActor {
 
     private List<String> servers = new ArrayList<>();
     private final Random random = new Random();
+
+    private String getRandomServer() {
+        App.print(String.valueOf(servers));
+        return this.servers.get(
+                random.nextInt(servers.size())
+        );
+    }
 }
