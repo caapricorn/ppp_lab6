79a02d7f950299454f1b50410b5c91b3d5889acf
caapricorn <caapricorn@mail.ru>
2021-12-18 03:41:54 +0300

    Scripted auto-commit on change (2021-12-18 03:41:54) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -1,5 +1,11 @@
 import akka.actor.AbstractActor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class StorageActor extends AbstractActor {
-    
+
+    private List<String> servers = new ArrayList<>();
+    private final Random random = new Random();
 }
