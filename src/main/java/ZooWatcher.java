7412535842d5e4ef2b4a926b91b30df5fc124e75
caapricorn <caapricorn@mail.ru>
2021-12-18 04:11:19 +0300

    Scripted auto-commit on change (2021-12-18 04:11:19) by gitwatch.sh

--- src/main/java/ZooWatcher.java
+++ src/main/java/ZooWatcher.java
@@ -1,5 +1,9 @@
+import akka.actor.ActorRef;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 public class ZooWatcher implements Watcher {
-    
+
+    private ZooKeeper zoo;
+    private ActorRef storage;
 }
