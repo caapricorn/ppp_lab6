a2eec1df753622a92b81fde5e8401df426682c37
caapricorn <caapricorn@mail.ru>
2021-12-18 03:45:25 +0300

    Scripted auto-commit on change (2021-12-18 03:45:25) by gitwatch.sh

--- src/main/java/StorageServer.java
+++ src/main/java/StorageServer.java
@@ -1,6 +1,12 @@
+import akka.actor.ActorRef;
+import akka.http.javadsl.Http;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 public class StorageServer implements Watcher {
 
-    public StorageServer
+    public StorageServer (Http http, ActorRef actorConfig, ZooKeeper zoo, String port) throws InterruptedException, KeeperException {
+        
+    }
 }
