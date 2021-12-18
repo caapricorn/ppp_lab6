fffb517dd737da2f4f19e5a9199c4254c5750e5c
caapricorn <caapricorn@mail.ru>
2021-12-18 04:12:41 +0300

    Scripted auto-commit on change (2021-12-18 04:12:41) by gitwatch.sh

--- src/main/java/ZooWatcher.java
+++ src/main/java/ZooWatcher.java
@@ -1,13 +1,27 @@
 import akka.actor.ActorRef;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ZooWatcher implements Watcher {
 
     private ZooKeeper zoo;
     private ActorRef storage;
+
+    private void sendServers() throws KeeperException, InterruptedException {
+        List<String> servers = new ArrayList<>();
+        for (String s : zoo.getChildren(SERVERS, this)) {
+            servers.add(new String(zoo.getData(SERVERS + "/" + s, false, null)));
+        }
+        storage.tell(
+                new MessageServersList(servers),
+                ActorRef.noSender()
+        );
+    }
+
 
     static class MessageServersList {
         private final List<String> servers;
