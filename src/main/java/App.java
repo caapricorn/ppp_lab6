9c5316f25b01cdd1700aa044546da34cdee78270
caapricorn <caapricorn@mail.ru>
2021-12-18 03:33:29 +0300

    Scripted auto-commit on change (2021-12-18 03:33:29) by gitwatch.sh

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,16 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.zookeeper.KeeperException;
+
+import java.io.IOException;
+
 public class App {
+
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+        BasicConfigurator.configure();
+        ActorSystem system = ActorSystem.create("routes");
+        ActorRef storage = system.actorOf(Props.create())
+    }
 }
