cfd72a579a3c8b8d1adfc637f8cdf0e3f4c77fa2
caapricorn <caapricorn@mail.ru>
2021-12-18 04:12:21 +0300

    Scripted auto-commit on change (2021-12-18 04:12:21) by gitwatch.sh

--- src/main/java/ZooWatcher.java
+++ src/main/java/ZooWatcher.java
@@ -2,10 +2,22 @@ import akka.actor.ActorRef;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.util.List;
+
 public class ZooWatcher implements Watcher {
 
     private ZooKeeper zoo;
     private ActorRef storage;
 
-    
+    static class MessageServersList {
+        private final List<String> servers;
+
+        MessageServersList(List<String> servers){
+            this.servers = servers;
+        }
+
+        public List<String> getServers() {
+            return servers;
+        }
+    }
 }
