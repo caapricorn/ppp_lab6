9e5db430bfac376ff44a4ac36ea4dd6220131bd9
caapricorn <caapricorn@mail.ru>
2021-12-18 03:45:44 +0300

    Scripted auto-commit on change (2021-12-18 03:45:44) by gitwatch.sh

--- src/main/java/StorageServer.java
+++ src/main/java/StorageServer.java
@@ -7,6 +7,8 @@ import org.apache.zookeeper.ZooKeeper;
 public class StorageServer implements Watcher {
 
     public StorageServer (Http http, ActorRef actorConfig, ZooKeeper zoo, String port) throws InterruptedException, KeeperException {
-        
+        this.http = http;
+        this.actorConfig = actorConfig;
+        this.zoo = zoo;
     }
 }
